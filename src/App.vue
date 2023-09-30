bd73f7f457159f62676b911a335af45a16eaa26c
tomot7 <tomotarou2020@outlook.jp>
2023-09-30 18:15:22 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Map/>
+  </div>
+</template>
+
+<script>
+import Map from './components/Leaflet_Map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Map
+  }
+}
+</script>
---
+++ src/components/Leaflet_Map.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="map">
+    <l-map :zoom="13" :center="[35.6895, 139.6917]">
+      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+      <l-marker :lat-lng="[35.6895, 139.6917]">
+        <l-icon
+          :icon-url="markerImg"
+          :shadow-url="markerShadowImg"
+        />      
+      </l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LIcon } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerImg from 'leaflet/dist/images/marker-icon.png';
+import markerShadowImg from 'leaflet/dist/images/marker-shadow.png';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LIcon  
+  },
+  data(){
+    return {
+      markerImg : markerImg,
+      markerShadowImg : markerShadowImg
+    }
+  },  
+}
+</script>
+
+<style scoped>
+#map {
+  height: 400px;
+  width: 100%;
+}
+</style>
